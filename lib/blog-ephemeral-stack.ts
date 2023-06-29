07604bcef2aa629dc0472255d267db053a2cb813
martzcodes <978362+martzcodes@users.noreply.github.com>
2023-06-29 10:44:29 -0400

    remove unused

--- lib/blog-ephemeral-stack.ts
+++ lib/blog-ephemeral-stack.ts
@@ -1,7 +1,5 @@
 import { DestroyMeStack, DestroyMeStackProps } from '@aws-community/ephemeral';
-import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class BlogEphemeralStack extends DestroyMeStack {
   constructor(scope: Construct, id: string, props: DestroyMeStackProps) {
